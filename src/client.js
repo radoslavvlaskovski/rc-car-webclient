59cfe841035f590c6e67eaab5f6beb3a76700635
radoslavvlaskovski <radoslav.vlaskovsky@gmail.com>
2018-06-11 19:32:11 +0200

    fix dependancy

--- src/client.js
+++ src/client.js
@@ -1,9 +1,9 @@
 
 var messages = require('./protos/client_pb');
 var services = require('./protos/client_grpc_pb');
+var grpc = require('grpc');
 
 function main() {
-  var grpc = require('grpc');
 
   var client = new services.CommandHandlerClient('localhost:50051',
                                           grpc.credentials.createInsecure());
