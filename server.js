13026b8d4df5769b74489c96b927e5d8158cfb8f
danieldimit <dani2dim@yahoo.com>
2018-06-20 02:11:33 +0200

    Added a controller GUI, added 2 new endpoints, forward left button working

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
+var bodyParser = require("body-parser");
 const app = express();
+const AUTH = true;
 var path = require('path');
-var DIST_DIR = path.join(__dirname, "dist");
+var basicAuth = require('express-basic-auth')
+var DIST_DIR = path.join(__dirname, "./src/");
+var direction = "still";
 
-app.use('/',express.static(path.join(__dirname, "")));
-app.use('/',express.static(path.join(__dirname, "/protos")));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(DIST_DIR));
+
+app.post('/setDirection',function(req,res){
+    direction = req.body.direction;
+    res.end("yes");
+});
+
+app.get('/getDirection',function(req,res){
+    res.send(direction);
+});
+
+
+
+if (AUTH) {
+    app.use(basicAuth({
+        //@lE><t0s|<0V
+        users: { 'admin': 'admin' },
+        challenge: true,
+        realm: 'Imb4T3st4pp'
+    }));
+}
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(DIST_DIR, "index.html"));
