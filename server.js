9a2e0f0865b8bb14f9259e5784b79721cd5331fc
danieldimit <dani2dim@yahoo.com>
2018-06-22 02:57:28 +0200

    Added controller with API, deleted protos and webpack for now

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const AUTH = true;
 var path = require('path');
 var basicAuth = require('express-basic-auth')
 var DIST_DIR = path.join(__dirname, "./src/");
-var direction = "still";
+var direction = "s";
+var d = new Date();
+var lastTimeUpdated = d.getTime();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,10 +15,19 @@ app.use(express.static(DIST_DIR));
 
 app.post('/setDirection',function(req,res){
     direction = req.body.direction;
+    console.log("direction: " + direction);
+    d = new Date();
+    lastTimeUpdated = d.getTime();
     res.end("yes");
 });
 
 app.get('/getDirection',function(req,res){
+    var tempDate = new Date();
+
+    console.log(tempDate.getTime(), " ",  lastTimeUpdated,"  ",tempDate.getTime() - lastTimeUpdated);
+    if (tempDate.getTime() - lastTimeUpdated > 2000) {
+        //direction = 's';
+    }
     res.send(direction);
 });
 
